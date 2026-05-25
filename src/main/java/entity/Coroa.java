4ecfd4824dfeb80472a8a6916012222e9629b660
Gustavo Vaz <gustavovaz278@gmail.com>
2026-05-24 21:17:40 -0300

    Update Coroa.java
    Adicionei a variavel real para ser usada na verificaçaõ da coroa

--- src/main/java/entity/Coroa.java
+++ src/main/java/entity/Coroa.java
@@ -3,6 +3,13 @@ package entity;
 public class Coroa {
     private String donoNome;
     private Boolean perdida;
+    private Boolean real;
+
+    public Coroa(String donoNome, Boolean perdida, Boolean real) {
+        this.donoNome = donoNome;
+        this.perdida = perdida;
+        this.real = real;
+    }
 
     public String getDonoNome() {
         return donoNome;
@@ -19,4 +26,10 @@ public class Coroa {
     public void setPerdida(Boolean perdida) {
         this.perdida = perdida;
     }
+
+    public Boolean getReal() {
+        return real;
+    }
+
+    // não tem setReal pq a coroa não pode mudar de falsa pra real ou real pra falsa
 }
